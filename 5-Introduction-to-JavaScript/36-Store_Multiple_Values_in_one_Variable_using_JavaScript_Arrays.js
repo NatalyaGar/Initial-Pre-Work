23a9a7d250454f3fcb29d8c15db40dce6fb5bd6e
Natalya Garusova <natalya.garusova@gmail.com>
2019-08-22 10:09:54 -0600

    Store multiple values in one variable using JavaScript arrays

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,6 @@
+// I learned store multiple values in one variable using JavaScript Arrays
+/ Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Anna", 22];
