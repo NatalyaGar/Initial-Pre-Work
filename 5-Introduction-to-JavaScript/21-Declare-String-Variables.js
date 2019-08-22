8171dbcc1a996d944ea35061d12da4845d15f888
Natalya Garusova <natalya.garusova@gmail.com>
2019-08-21 18:24:46 -0600

    Declare string variables

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,8 @@
+// I learned how to declare string variables
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Natalya";
+var myLastName = "Garusova";
