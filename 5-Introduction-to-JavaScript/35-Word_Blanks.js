c4ab5417eec549fb559a50874d144ceb982dcd0b
Natalya Garusova <natalya.garusova@gmail.com>
2019-08-22 09:24:17 -0600

    Used word blanks

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,14 @@
+// I learned how use word blanks
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "";
+   result += "This "+myNoun+" is "+myAdjective+" he "+myVerb+" very "+myAdverb+".";
+
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("bird", "small", "fly", "nicely");
+// wordBlanks("dog", "big", "ran", "quickly");
