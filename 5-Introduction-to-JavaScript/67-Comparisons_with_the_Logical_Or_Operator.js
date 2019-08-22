57becb829f32c443b93a59372adfd65561726166
Natalya Garusova <natalya.garusova@gmail.com>
2019-08-22 15:28:50 -0600

    Comparisons with the logical or operator

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,16 @@
+// I learned comparisons with the logical or operator
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val > 20 || val < 10  ) {
+    return "Outside";
+  }
+
+
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(9);
