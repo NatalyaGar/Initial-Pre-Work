fae53bfeb588c895c17b2fcb90cfcb3d6394f164
Natalya Garusova <natalya.garusova@gmail.com>
2019-08-23 16:40:50 -0600

    Manipulating complex objects

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,27 @@
+// I learned manipulating complex objects
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+
+   {
+  "artist": "Madonna",
+    "title": "Dance",
+    "release_year": 1980,
+    "formats": [
+      "8T",
+      "LP"
+    ],
+
+    }
+
+  // Add record here
+];
