ea0457e5198925cefb52566da1bf192d66764111
Natalya Garusova <natalya.garusova@gmail.com>
2019-08-23 14:18:54 -0600

    Build JavaScript Objects

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,20 @@
+// I learned how to build JavaScript objects
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+ "name": "Sharik",
+ "legs": 4,
+ "tails": 1,
+ "friends": ["Mishka", "Trizor", "Djuli"]
+
+
+
+};
