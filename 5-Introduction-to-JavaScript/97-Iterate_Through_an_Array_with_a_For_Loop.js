459499cf34386b8392b646234bb24ede87049f8f
Natalya Garusova <natalya.garusova@gmail.com>
2019-08-26 10:39:17 -0600

    Iterate through an array with a for loop

---
+++ 5-Introduction-to-JavaScript/97-Iterate_Through_an_Array_with_a_For_Loop.js
@@ -0,0 +1,9 @@
+
+// I learned iterate through an array with a for loop
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+var total = 0;
+// Only change code below this line
+for (var i = 0; i < myArr.length; i++){
+  total += myArr[i];
+}
