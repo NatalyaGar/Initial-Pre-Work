07eefa174a574d223f51c502007d33320d067f0f
Natalya Garusova <natalya.garusova@gmail.com>
2019-08-26 09:59:43 -0600

    For loops iterate with JavaScript

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,15 @@
+// I learned for loops iterate with JavaScriopt 
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for(var i = 1; i <= 5; i++) {
+  myArray.push(i);
+}
