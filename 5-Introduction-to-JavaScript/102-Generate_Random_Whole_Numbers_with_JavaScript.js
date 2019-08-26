3a2aa6bb3f25506e5669504ec40d9cecfeaf7363
Natalya Garusova <natalya.garusova@gmail.com>
2019-08-26 12:19:09 -0600

    Generate random whole numbers with JavaScript

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,9 @@
+// I learned generate random whole numbers with JavaScript
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum(myMin, myMax) {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10 );
+};
