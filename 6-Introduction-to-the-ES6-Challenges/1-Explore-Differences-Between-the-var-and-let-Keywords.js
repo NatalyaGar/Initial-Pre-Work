f5f19f56fbdcf14806a49e67b4cb72aa7f09a88e
Natalya Garusova <natalya.garusova@gmail.com>
2019-08-26 13:22:21 -0600

    Explore differences between the var and let keywords

---
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -0,0 +1,11 @@
+// I learned differevces between the var and let keywords
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
