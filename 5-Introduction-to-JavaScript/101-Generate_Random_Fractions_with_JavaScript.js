90987755764cf224a9d6ea59d558fabfcb54ebb5
Natalya Garusova <natalya.garusova@gmail.com>
2019-08-26 11:56:51 -0600

    Generate random fractions with JavaScript

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,9 @@
+// I learned ganerate Random fractions with JavaScript
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
