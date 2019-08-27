f1ab6822ee954530b8f7978a74c884964f37e821
Natalya Garusova <natalya.garusova@gmail.com>
2019-08-26 19:47:03 -0600

    Write declarative functions with ES6

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,13 @@
+// I learned write concise declarative functions with ES6
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear (newGear) {
+    "use strict";
+    this.gear= newGear;
+
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
